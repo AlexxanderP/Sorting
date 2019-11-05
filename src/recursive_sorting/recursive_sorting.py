9fad4177ce500cd25636288f8c4fea0b85250dc5
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-11-05 14:36:01 -0800

    Recursive test passing with OK, going back through to write notes for myself explaining the steps

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,8 +3,31 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+
+    a, b, c = 0, 0, 0
+    while a < len(arrA) and b < len(arrB):
+        if arrA[a] < arrB[b]:
+            merged_arr[c] = arrA[a]
+            a += 1
+            c += 1
+        else:
+            merged_arr[c] = arrB[b]
+            b += 1
+            c += 1
+    while a < len(arrA):
+        merged_arr[c] = arrA[a]
+        a += 1
+        c += 1
+    while b < len(arrB):
+        merged_arr[c] = arrB[b]
+        b += 1
+        c += 1
     return merged_arr
+
+# Checking Merge in console
+l1 = [2, 4, 6, 8, 10]
+l2 = [1, 5, 9]
+print(f"merged list: {merge(l1, l2)}")
 
 
 #                [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
@@ -26,7 +49,13 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) <= 1:
+        return arr
 
+
+    left = merge_sort(arr[0: len(arr) // 2])
+    right = merge_sort(arr[len(arr) // 2:])
+    arr = merge(left,right)
     return arr
 
 
